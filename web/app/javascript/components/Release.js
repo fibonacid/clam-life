4c0eabf42f23653ed58616bf0c76d51576f6d47d
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-04-19 18:51:45 +0200

    chore: toggle cover visibility on click

--- web/app/javascript/components/Release.js
+++ web/app/javascript/components/Release.js
@@ -13,6 +13,33 @@ class _Release {
   setupEvents() {
     // Fade in cover image as soon it's loaded.
     imagesLoaded(".release__cover-inner", onImageLoaded);
+
+    const containers = document.querySelectorAll(".release");
+
+    [...containers].forEach(function(container) {
+      const cover = container.querySelector(".release__cover");
+
+      // When user clicks on the cover:
+      cover.addEventListener("click", function() {
+        // Hide the cover.
+        cover.classList.add("release__cover--hidden");
+      });
+
+      // When user clicks outside
+      document.addEventListener("click", function(event) {
+        let targetElement = event.target;
+        do {
+          if (targetElement == cover) {
+            return;
+          }
+          // Go up the DOM
+          targetElement = targetElement.parentNode;
+        } while (targetElement);
+
+        // Outside click !
+        cover.classList.remove("release__cover--hidden");
+      });
+    });
   }
 }
 
