6e51f41453d723811e7a9c7d8f97874883832a8a
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-04-19 21:13:45 +0200

    chore: toggle visibility of links

--- web/app/javascript/components/Header.js
+++ web/app/javascript/components/Header.js
@@ -9,7 +9,36 @@ class _Header {
   }
 
   setupEvents() {
-    console.log("hello from header");
+    const showLinks = document.querySelector(".header__show-links");
+    const links = document.querySelector(".header__social");
+    console.log(links);
+
+    if (showLinks && links) {
+      showLinks.addEventListener("click", function() {
+        links.classList.remove("header__social--hidden");
+        showLinks.classList.add("header__show-links--hidden");
+      });
+
+      // When user clicks outside
+      document.addEventListener("click", function(event) {
+        let targetElement = event.target;
+        let clickedInside = false;
+        do {
+          if (targetElement == showLinks) {
+            clickedInside = true;
+            return;
+          }
+          // Go up the DOM
+          targetElement = targetElement.parentNode;
+        } while (targetElement);
+
+        // Outside click !
+        if (!clickedInside) {
+          links.classList.add("header__social--hidden");
+          showLinks.classList.remove("header__show-links--hidden");
+        }
+      });
+    }
   }
 }
 
