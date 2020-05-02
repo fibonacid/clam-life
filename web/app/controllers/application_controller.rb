b415be47632c96fefc60a58f187c00fd75f4a4e9
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-05-02 17:48:48 +0200

    chore: get dashboard before every action

--- web/app/controllers/application_controller.rb
+++ web/app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   include ::Binda::MaintenanceHelpers
+
+  before_action :set_variables
+
+  def set_variables
+    @dashboard = B.get_boards('dashboard').includes(:images).first;
+  end
 end
