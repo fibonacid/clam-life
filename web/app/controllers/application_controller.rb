169cf9efd03a6723a26c08f67c5fbc9e9342433a
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-05-02 18:39:07 +0200

    chore: get contacts board

--- web/app/controllers/application_controller.rb
+++ web/app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
 
   def set_variables
     @dashboard = B.get_boards('dashboard').includes(:images).first;
+    @contacts = B.get_boards('contacts').first;
   end
 end
