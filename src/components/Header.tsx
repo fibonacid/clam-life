9d79ec61764d1891ff04ce6541c1d9189c733de3
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-09-10 18:55:20 +0200

    run script on mount

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,7 +18,7 @@ const links: Link[] = [
 
 export default function Header() {
   onMount(() => {
-    console.log("hello from header");
+    init();
   });
   return (
     <header class="header">
@@ -37,4 +37,29 @@ export default function Header() {
       </div>
     </header>
   );
+}
+
+function init() {
+  const showLinks = document.querySelector(".header__show-links");
+  const links = document.querySelector(".header__social");
+
+  if (showLinks && links) {
+    showLinks.addEventListener("click", function () {
+      links.classList.remove("header__social--hidden");
+      showLinks.classList.add("header__show-links--hidden");
+    });
+
+    // Listen for all clicks on the document
+    document.addEventListener(
+      "click",
+      function (event: any) {
+        // If the click happened inside the container, bail
+        if (event.target.closest(".header__show-links")) return;
+        // Otherwise, show the cover
+        links.classList.add("header__social--hidden");
+        showLinks.classList.remove("header__show-links--hidden");
+      },
+      false
+    );
+  }
 }
