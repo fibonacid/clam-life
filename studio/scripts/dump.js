63da130f0012c6ad31b6e49815c76b4529edf2ed
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-09-13 21:03:42 +0200

    add images to script

--- studio/scripts/dump.js
+++ studio/scripts/dump.js
@@ -1,5 +1,7 @@
 import releases from "./data/releases.json";
 import sanityClient from "part:@sanity/base/client";
+import { resolve, join } from "path";
+import fs from "fs";
 
 const client = sanityClient.withConfig({
   apiVersion: "2022-06-16",
@@ -7,10 +9,43 @@ const client = sanityClient.withConfig({
 
 const transaction = client.transaction();
 
+const ImageMap = new Map();
+
+const publicFolder = resolve("../public/");
+
+const uploadImage = async (path) => {
+  if (ImageMap.has(path)) {
+    return ImageMap.get(path);
+  } else {
+    const doc = await client.assets.upload(
+      "image",
+      fs.createReadStream(join(publicFolder, path)),
+      {
+        preserveOriginalFilename: true,
+      }
+    );
+    console.log(doc);
+    ImageMap.set(path, doc);
+    return doc;
+  }
+};
+
+const getImage = (path) => {
+  return ImageMap.get(path);
+};
+
 (async function main() {
+  for await (const release of releases) {
+    await uploadImage(release.releaseImage);
+    await uploadImage(release.coverImage);
+  }
+
   //////////////////////////
   releases.forEach((release) => {
     const id = "release-" + release.catalogNumber;
+    const releaseImageRef = getImage(release.releaseImage)._id;
+    const coverImageRef = getImage(release.coverImage)._id;
+
     transaction.createOrReplace(
       {
         _id: id,
@@ -21,7 +56,18 @@ const transaction = client.transaction();
         artistName: release.artistName,
         releaseType: release.releaseType,
         buyLink: release.buyLink,
-        // releaseImage: null,
+        coverImage: {
+          asset: {
+            _type: "image",
+            _ref: coverImageRef,
+          },
+        },
+        releaseImage: {
+          asset: {
+            _type: "image",
+            _ref: releaseImageRef,
+          },
+        },
         // coverImage: null,
         tracklist: {
           _type: "tracklist",
