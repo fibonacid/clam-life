c4ae86b11d3e69151b9169f02a065d9f2ecdda6a
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-09-13 22:33:04 +0200

    add astro image

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
 import vercel from "@astrojs/vercel/serverless";
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()],
+  integrations: [solidJs(), image()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel()
 });
