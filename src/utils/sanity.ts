9afd84468d9339114389f71b22258fd2c6393660
Lorenzo Rivosecchi <lorenzo.rivosecchi@lacolonia.studio>
2022-11-04 22:57:31 +0100

    use cdn

--- src/utils/sanity.ts
+++ src/utils/sanity.ts
@@ -4,6 +4,5 @@ export const client = new PicoSanity({
   projectId: import.meta.env.SANITY_PROJECT_ID as string,
   dataset: import.meta.env.SANITY_DATASET || "production",
   apiVersion: "2021-03-25", // use a UTC date string
-  useCdn: false,
-  token: import.meta.env.SANITY_API_TOKEN,
+  useCdn: true,
 });
